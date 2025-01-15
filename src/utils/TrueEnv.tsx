06510dab22c380af1c155a504c95e80cff89bc1c
Adria Guilera <adriaguilera7@gmail.com>
2025-01-15 22:51:49 +0100

    Env variables

--- src/utils/TrueEnv.tsx
+++ src/utils/TrueEnv.tsx
@@ -1,18 +1,11 @@
-import fakeEnv from './fakeEnv';
 
-const debug = true;
 export default function TrueEnv() {
-    if(!debug){
-        const GH_KEY = process.env.REACT_APP_GITHUB_KEY;
-        return {
-        method: 'GET',
-        headers: {
-            Authorization: `Bearer ${GH_KEY}`,
-            'X-GitHub-Api-Version': '2022-11-28',
-        }
-        };
+    const GH_KEY = import.meta.env.VITE_GITHUB_KEY;
+    return {
+    method: 'GET',
+    headers: {
+        Authorization: `Bearer ${GH_KEY}`,
+        'X-GitHub-Api-Version': '2022-11-28',
     }
-    else{
-        return fakeEnv();
-    }
-  }
+    };
+}
